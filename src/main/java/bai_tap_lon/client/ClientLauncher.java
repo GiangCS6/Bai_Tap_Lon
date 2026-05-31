27220fd058570b1c3702be5ad01ec5714c7ea722
Vo Manh Cuong <25021665@vnu.edu.vn>
2026-05-31 21:33:06 +0700

    Cấu hình build file JAR cho Server và Client để nộp đồ án

---
+++ src/main/java/bai_tap_lon/client/ClientLauncher.java
@@ -0,0 +1,7 @@
+package bai_tap_lon.client;
+
+public class ClientLauncher {
+    public static void main(String[] args) {
+        ClientStart.main(args);
+    }
+}
---
+++ src/main/java/bai_tap_lon/server/network/ServerLauncher.java
@@ -0,0 +1,7 @@
+package bai_tap_lon.server.network;
+
+public class ServerLauncher {
+    public static void main(String[] args) {
+        ServerStart.main(args);
+    }
+}
